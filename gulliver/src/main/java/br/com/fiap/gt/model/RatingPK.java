c4886e1643ec8987f280bd4826d8d8364e0a6792
kmitsuishi-08 <61333839+kmitsuishi-08@users.noreply.github.com>
2021-06-07 21:34:48 -0300

    add config and fix errors

--- gulliver/src/main/java/br/com/fiap/gt/model/RatingPK.java
+++ gulliver/src/main/java/br/com/fiap/gt/model/RatingPK.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 public class RatingPK implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	private int id;
 	private int rentalCompany;
 	private int user;
