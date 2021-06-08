12bbf1dba690510b1b25629079c01329f30eb810
gu807 <gugamelo2@gmail.com>
2021-06-08 17:51:57 -0300

    teste

--- gulliver/src/main/java/br/com/fiap/gt/beans/UserBean.java
+++ gulliver/src/main/java/br/com/fiap/gt/beans/UserBean.java
@@ -2,13 +2,22 @@ package br.com.fiap.gt.beans;
 
 import javax.enterprise.context.RequestScoped;
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> 45d8072 (teste)
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 >>>>>>> f818b41 (Login adicionado)
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 
+=======
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import br.com.fiap.gt.dao.UserDao;
+>>>>>>> 03cf525 (teste)
 import br.com.fiap.gt.dao.impl.UserDaoImpl;
 import br.com.fiap.gt.model.User;
 import br.com.fiap.gt.singleton.EntityManagerFactorySingleton;
@@ -17,6 +26,7 @@ import br.com.fiap.gt.singleton.EntityManagerFactorySingleton;
 @RequestScoped
 public class UserBean {
 	
+<<<<<<< HEAD
 <<<<<<< HEAD
 	EntityManager em = EntityManagerFactorySingleton.getInstance().createEntityManager(); 
 
@@ -28,6 +38,8 @@ public class UserBean {
 		this.user = new User();
 		return "index?faces-redirect=true";
 =======
+=======
+>>>>>>> 45d8072 (teste)
 	EntityManager em = EntityManagerFactorySingleton.getInstance().createEntityManager();
 	private User user = new User();
 	
@@ -43,7 +55,21 @@ public class UserBean {
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Invalido!", "Erro"));
 			return "login?faces-redirect=true";
 		}
+<<<<<<< HEAD
 >>>>>>> f818b41 (Login adicionado)
+=======
+=======
+	private EntityManager em = EntityManagerFactorySingleton.getInstance().createEntityManager(); 
+
+	private User user = new User();
+	
+	public String save() {
+		UserDao uDao = new UserDaoImpl(em);
+		uDao.create(this.user);
+		this.user = new User();
+		return "index?faces-redirect=true";
+>>>>>>> 03cf525 (teste)
+>>>>>>> 45d8072 (teste)
 	}
 
 	public User getUser() {
@@ -57,8 +83,15 @@ public class UserBean {
 	
 	
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 	
 
 >>>>>>> f818b41 (Login adicionado)
+=======
+	
+
+=======
+>>>>>>> 03cf525 (teste)
+>>>>>>> 45d8072 (teste)
 }
